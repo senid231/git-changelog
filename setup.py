610f7e47c413a00ba4c58b82a354c11846075b3f
Denis Talakevich <cnpaut@gmail.com>
2017-04-11 17:22:57 +0300

    #1 dry and refactor code, add args responding and help usage
    add args for each parameter that can be configured
    
    add help with human readable usage
    
    add arguments for auto commit, skip prompts, suppress output, debug output and some more

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from setuptools import setup
 import git_changelog
 
 setup(
-    name='git_changelog',
+    name="git_changelog",
     version=git_changelog.__version__,
-    packages=['git_changelog'],
-    # scripts=['bin/changelog-git'],
-    url='https://github.com/senid231/git-changelog',
-    license='MIT',
-    author='Denis Talakevich',
-    author_email='senid231@gmail.com',
-    description='appends changelog using git commits',
-    install_requires=['tzlocal', 'GitPython'],
+    packages=["git_changelog"],
+    url="https://github.com/senid231/git-changelog",
+    license="MIT",
+    author="Denis Talakevich",
+    author_email="senid231@gmail.com",
+    description="appends changelog using git commits",
+    install_requires=["tzlocal", "GitPython"],
     entry_points={
-        'console_scripts':
-            ['changelog-git = git_changelog.GitChangelog:append_to_changelog']
+        "console_scripts":
+            ["changelog-git = git_changelog.command_line:main"]
     }
 
 )
